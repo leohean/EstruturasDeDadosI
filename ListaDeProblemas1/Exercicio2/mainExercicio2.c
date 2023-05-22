9386bd374cb7f41e92c0147cf06bcb1d31f3418e
Leonardo <leohean_cardoso@hotmail.com>
2023-05-22 20:25:23 -0300

    Exercicio 3-4 finalizado

--- ListaDeProblemas1/Exercicio2/mainExercicio2.c
+++ ListaDeProblemas1/Exercicio2/mainExercicio2.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "exercicio2.h"
 
 int main(){
-    
+    NohDuplo *lista=NULL;
+    insereOrdenado(4, &lista);
+    insereOrdenado(1, &lista);
+    insereOrdenado(-1, &lista);
+    insereOrdenado(19, &lista);
+    insereOrdenado(14, &lista);
+    insereOrdenado(-10000, &lista);
+
+    imprimir(lista);
 }
